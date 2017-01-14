586068fb41f85a8443b44a5761bcdff3a5b65810
Gavin McWhinnie <gavinthegav@gmail.com>
2017-01-14 18:53:38 +0000

    Continued work on the planet

--- earth.js
+++ earth.js
@@ -41,18 +41,24 @@ for(i=0; i < 10; i++) {
         case 5:
             div5.style.width = '250px';
             div5.style.left = '0px';
+            div5.style.background = 'linear-gradient(115deg, transparent 30% , blue 30%, blue 40%, transparent 40%,\
+            transparent 80%, blue 80%, blue 90%, transparent 90%), linear-gradient(180deg, blue 50%, green 50%)';
             break;
         case 6:
             div6.style.width = '225px';
             div6.style.left = '12.5px';
+            div6.style.background = 'linear-gradient(115deg, green 25%, blue 25%, blue 45%, green 45%, green 80%, blue 80%,\
+            blue 95%, green 95%)';
             break;
         case 7:
             div7.style.width = '200px';
             div7.style.left = '25px';
+            div7.style.background = 'linear-gradient(115deg, green 20%, blue 20%, blue 55%, green 55%, green 75%, blue 75%)';
             break;
         case 8:
             div8.style.width = '150px';
             div8.style.left = '50px';
+            div8.style.background = 'linear-gradient(115deg, green 10%, blue 10%, blue 60%, green 60%, green 75%, blue 75%)';
             break;
         case 9:
             div9.style.width = '75px';
